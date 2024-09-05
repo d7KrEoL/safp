570c1eec26104e1b4aa547d6d11d0c36e92318d9
d7KrEoL <74565655+d7KrEoL@users.noreply.github.com>
2024-09-05 11:51:27 +0300

    Update safp.lua
    SaveFile update

--- moonloader/lib/safp.lua
+++ moonloader/lib/safp.lua
@@ -1,4 +1,4 @@
---Can read *.safp flight plan data
+--San Andreas Flight Plan format parsing lib
 --You can easily generate flight plan here: http://sampmap.ru/samap
 
 local wpData = {wpid, pos = {x, y, z}}
@@ -7,10 +7,29 @@ function LoadFile(path)
 	for rw in ipairs(wpData) do
 		wpData[rw] = nil;
 	end
-  
 	local rsline = getFileDataLines(path)
 	parseData(rsline)
 	return wpData
+end
+
+function SaveFile(path, data)
+	local file=io.open(path,"w+")
+	if file==nil then 
+		print("{FF0000}Error: unable to save flight plan ({FFFFFF}" .. arg .. "{FF0000})")
+		return false
+	end
+	file:write("San Andreas Flight Plan file encoder ver. 5.8.24-lua\n")
+	file:write("This file can be read by SAMP Avionics LUA script: https://github.com/d7KrEoL/avionics\n")
+	file:write("You can simply edit this file at online flight plan editor: http://sampmap.ru/samap\n")
+	for i = 1, #data do
+		file:write(string.format("{Waypoint=%s;PosX=%s;PosY=%s;PosZ=%s}\n", 
+									data[i].number, replace(tostring(data[i].x), '%.', ','), 
+									replace(tostring(data[i].y), '%.', ','), 
+									replace(tostring(data[i].z), '%.', ',')))
+	end
+	file:write(string.format("Generated %s.%s.%s at %s:%s:%s\n", os.date('%d'), os.date('%m'), os.date('%Y'), os.date('%H'),os.date('%M'), os.date('%S')))
+	file:close()
+	return true
 end
 
 function doesFileExist(path)
@@ -40,6 +59,7 @@ function parseData(lines)
 		if #splitstring > 2 then
 			count = count + 1
 			table.insert(wpData, {wpid = 0, pos = {x = 0, y = 0, z = 0}})
+		
 			for j = 1, #splitstring do
 				lineType = parseLine(splitstring[j], lineType, count)
 			end
@@ -69,23 +89,20 @@ function parseLine(splitstring, lineType, index)
 end
 
 function split (inputstr, sep)
-  if sep == nil then
-          sep = "%s"
-  end
-  local t={}
-  for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
-          table.insert(t, str)
-  end
-  return t
+        if sep == nil then
+                sep = "%s"
+        end
+        local t={}
+        for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+                table.insert(t, str)
+        end
+        return t
 end
 
 function replace (inputstr, from, to)
-	if (inputstr == nil) or (from == nil) then 
-    print("Replace null exception: ", inputstr, from, to) 
-    return 
-  end
+	if (inputstr == nil) or (from == nil) then print("{FF0000}Replace null exception: {FFFFFF}", inputstr, from, to) return end
 	local res = string.gsub(inputstr, from, to)
 	return res
 end
 
-return { Load = LoadFile, Waypoints = wpData }
+return { Load = LoadFile, Save = SaveFile, Waypoints = wpData }
